098ebfc238db3194e87b44c1231d16407441764c
Yan Liu <yanliuse7@gmail.com>
2022-04-04 16:36:40 -0400

    Fix background animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import MainPage from './pages/MainPage/MainPage';
 import TeamsPage from './pages/TeamsPage/TeamsPage';
@@ -11,63 +10,28 @@ import bg5 from './assets/images/stadium-5.jpg';
 import bg6 from './assets/images/stadium-6.jpg';
 import './App.scss';
 
-class App extends Component {
-  state = {
-    currentIndex: 0,
-    backgroundImages: [bg1, bg2, bg3, bg4, bg5, bg6]
-  }
-
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => {
-        this.changeBackground();
-      },
-      4000
-    );
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-
-  nextIndex(currentIndex) {
-    return (currentIndex + 1) % 6;
-  }
-
-  changeBackground() {
-    this.setState(state => ({
-      currentIndex: this.nextIndex(state.currentIndex)
-    }));
-  }
-
-  render() {
-    const { currentIndex, backgroundImages } = this.state;
-    return (
-      <Router>
-        <div className="wrapper">
-          <div className="wrapper__background">
-            <img
-              src={backgroundImages[currentIndex]}
-              alt="background"
-              className="wrapper__background-top"
-            />
-            <img
-              src={backgroundImages[this.nextIndex(currentIndex)]}
-              alt="background"
-              className="wrapper__background-bottom"
-            />
-          </div>
-          <div className="wrapper__main">
-            <Switch>
-              <Route path="/" exact component={MainPage} />
-              <Route path="/teams" exact component={TeamsPage} />
-              <Route path="/teams/:teamID" component={TeamInfo} />
-            </Switch>
-          </div>
+function App() {
+  return (
+    <Router>
+      <div className="wrapper">
+        <div className="wrapper__background">
+          <img src={bg6} alt="background-6" />
+          <img src={bg5} alt="background-5" />
+          <img src={bg4} alt="background-4" />
+          <img src={bg3} alt="background-3" />
+          <img src={bg2} alt="background-2" />
+          <img src={bg1} alt="background-1" />
         </div>
-      </Router>
-    );
-  }
+        <div className="wrapper__main">
+          <Switch>
+            <Route path="/" exact component={MainPage} />
+            <Route path="/teams" exact component={TeamsPage} />
+            <Route path="/teams/:teamID" component={TeamInfo} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
