8f9611b1b05f63d325debc9d809f0fd3c9d04912
Yan Liu <yanliuse7@gmail.com>
2022-04-04 10:29:54 -0400

    Add MainPage

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,27 @@
+import './MainPage.scss';
+
+function MainPage() {
+  return (
+    <section className="main">
+      <div className="main__title-box">
+        <h1 className="main__title">
+          Welcome to FIFA World Cup Emulator
+        </h1>
+      </div>
+      <div className="main__options">
+        <div className="button">
+          <p className="button__text">
+            view / edit teams
+          </p>
+        </div>
+        <div className="button">
+          <p className="button__text">
+            play matches
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default MainPage;
