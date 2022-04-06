676441f08e9128149b415f121d4fedf2021852ae
Yan Liu <yanliuse7@gmail.com>
2022-04-06 00:50:59 -0400

    Update group stage game simulation process

--- src/components/TournamentMatch/TournamentMatch.js
+++ src/components/TournamentMatch/TournamentMatch.js
@@ -48,20 +48,57 @@ class GroupStage {
         });
       }
     });
-  }
-
-  static shuffle(array) {
-    for (let i = 1; i < array.length; ++i) {
-      let j = Number.parseInt(Math.random() * i);
-      const temp = {...array[i]};
-      array[i] = array[j];
-      array[j] = temp;
-    }
-    return array;
-  }
-
-  startGame() {
-    const res = {};
+    this.schedule = [
+      [['A', '1'], ['A', '2']],
+      [['A', '3'], ['A', '4']],
+      [['B', '1'], ['B', '2']],
+      [['B', '3'], ['B', '4']],
+      [['C', '1'], ['C', '2']],
+      [['C', '3'], ['C', '4']],
+      [['D', '1'], ['D', '2']],
+      [['D', '3'], ['D', '4']],
+      [['E', '1'], ['E', '2']],
+      [['E', '3'], ['E', '4']],
+      [['F', '1'], ['F', '2']],
+      [['F', '3'], ['F', '4']],
+      [['G', '1'], ['G', '2']],
+      [['G', '3'], ['G', '4']],
+      [['H', '1'], ['H', '2']],
+      [['H', '3'], ['H', '4']],
+      [['A', '1'], ['A', '3']],
+      [['A', '2'], ['A', '4']],
+      [['B', '1'], ['B', '3']],
+      [['B', '2'], ['B', '4']],
+      [['C', '1'], ['C', '3']],
+      [['C', '2'], ['C', '4']],
+      [['D', '1'], ['D', '3']],
+      [['D', '2'], ['D', '4']],
+      [['E', '1'], ['E', '3']],
+      [['E', '2'], ['E', '4']],
+      [['F', '1'], ['F', '3']],
+      [['F', '2'], ['F', '4']],
+      [['G', '1'], ['G', '3']],
+      [['G', '2'], ['G', '4']],
+      [['H', '1'], ['H', '3']],
+      [['H', '2'], ['H', '4']],
+      [['A', '1'], ['A', '4']],
+      [['A', '3'], ['A', '2']],
+      [['B', '1'], ['B', '4']],
+      [['B', '3'], ['B', '2']],
+      [['C', '1'], ['C', '4']],
+      [['C', '3'], ['C', '2']],
+      [['D', '1'], ['D', '4']],
+      [['D', '3'], ['D', '2']],
+      [['E', '1'], ['E', '4']],
+      [['E', '3'], ['E', '2']],
+      [['F', '1'], ['F', '4']],
+      [['F', '3'], ['F', '2']],
+      [['G', '1'], ['G', '4']],
+      [['G', '3'], ['G', '2']],
+      [['H', '1'], ['H', '4']],
+      [['H', '3'], ['H', '2']],
+    ];
+    this.allStandings = {};
     for (const group in this.teams) {
       // init standings
       const standings = {}
@@ -76,58 +113,28 @@ class GroupStage {
           'GD': 0
         };
       }
-      for (const i in this.teams[group]) {
-        const teamX = this.teams[group][i];
-        for (const j in this.teams[group]) {
-          if (i < j) {
-            const teamY = this.teams[group][j];
-            const engine = new GameEngine(
-              {
-                'squad': this.players[group][i],
-                'formation': this.formations[group][i],
-                'teamName': teamX.teamName,
-                'ranking': teamX.teamID
-              },
-              {
-                'squad': this.players[group][j],
-                'formation': this.formations[group][j],
-                'teamName': teamY.teamName,
-                'ranking': teamY.teamID
-              },
-              false,
-              true
-            );
-            const score = engine.startGame();
-            const { regularScore : rs } = score;
-            if (rs.home < rs.away) {
-              standings[teamX.teamName].L += 1;
-              standings[teamY.teamName].W += 1;
-              standings[teamY.teamName].pts += 3;
-            } else if (rs.home > rs.away) {
-              standings[teamX.teamName].W += 1;
-              standings[teamX.teamName].pts += 3;
-              standings[teamY.teamName].L += 1;
-            } else {
-              standings[teamX.teamName].D += 1;
-              standings[teamX.teamName].pts += 1;
-              standings[teamY.teamName].D += 1;
-              standings[teamY.teamName].pts += 1;
-            }
-            standings[teamX.teamName].GF += rs.home;
-            standings[teamX.teamName].GA += rs.away;
-            standings[teamX.teamName].GD += rs.home - rs.away;
-            standings[teamY.teamName].GF += rs.away;
-            standings[teamY.teamName].GA += rs.home;
-            standings[teamY.teamName].GD += rs.away - rs.home;
-            console.log(teamX.teamName, rs.home, ':', rs.away, teamY.teamName);
-          }
-        }
-      }
+      this.allStandings[group] = standings;
+    }
+  }
+
+  static shuffle(array) {
+    for (let i = 1; i < array.length; ++i) {
+      let j = Number.parseInt(Math.random() * i);
+      const temp = {...array[i]};
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  }
+
+  calcStandings() {
+    const res = {};
+    for (const group in this.allStandings) {
       const finalStandings = [];
-      for (const team in standings) {
+      for (const team in this.allStandings[group]) {
         finalStandings.push({
           teamName: team,
-          ...standings[team]
+          ...this.allStandings[group][team]
         });
       }
       finalStandings.sort((teamX, teamY) => {
@@ -149,11 +156,65 @@ class GroupStage {
     }
     return res;
   }
+
+  nextGame(matchID) {
+    const group = this.schedule[matchID][0][0];
+    const i = this.schedule[matchID][0][1];
+    const j = this.schedule[matchID][1][1];
+    const teamX = this.teams[group][i];
+    const teamY = this.teams[group][j];
+    const engine = new GameEngine(
+      {
+        'squad': this.players[group][i],
+        'formation': this.formations[group][i],
+        'teamName': teamX.teamName,
+        'ranking': teamX.teamID
+      },
+      {
+        'squad': this.players[group][j],
+        'formation': this.formations[group][j],
+        'teamName': teamY.teamName,
+        'ranking': teamY.teamID
+      },
+      false,
+      true
+    );
+    const score = engine.startGame();
+    const { regularScore : rs } = score;
+    if (rs.home < rs.away) {
+      this.allStandings[group][teamX.teamName].L += 1;
+      this.allStandings[group][teamY.teamName].W += 1;
+      this.allStandings[group][teamY.teamName].pts += 3;
+    } else if (rs.home > rs.away) {
+      this.allStandings[group][teamX.teamName].W += 1;
+      this.allStandings[group][teamX.teamName].pts += 3;
+      this.allStandings[group][teamY.teamName].L += 1;
+    } else {
+      this.allStandings[group][teamX.teamName].D += 1;
+      this.allStandings[group][teamX.teamName].pts += 1;
+      this.allStandings[group][teamY.teamName].D += 1;
+      this.allStandings[group][teamY.teamName].pts += 1;
+    }
+    this.allStandings[group][teamX.teamName].GF += rs.home;
+    this.allStandings[group][teamX.teamName].GA += rs.away;
+    this.allStandings[group][teamX.teamName].GD += rs.home - rs.away;
+    this.allStandings[group][teamY.teamName].GF += rs.away;
+    this.allStandings[group][teamY.teamName].GA += rs.home;
+    this.allStandings[group][teamY.teamName].GD += rs.away - rs.home;
+    return {
+      'home': teamX.teamName,
+      'homeScore': rs.home,
+      'away': teamY.teamName,
+      'awayScore': rs.away
+    };
+  }
 }
 
 class TournamentMatch extends Component {
   state = {
-    standings: {}
+    standings: {},
+    nextGroupStageGame: 0,
+    groupMatchResult: []
   }
 
   componentDidMount() {
@@ -167,7 +228,9 @@ class TournamentMatch extends Component {
       .then(res => {
         playersData = res.data;
         this.setState({
-          GroupStage: new GroupStage(teamsData, playersData)
+          teamsData,
+          playersData,
+          groupStage: new GroupStage(teamsData, playersData)
         });
       })
       .catch(err => {
@@ -176,11 +239,34 @@ class TournamentMatch extends Component {
   }
 
   handleStartGroupStage = () => {
-    const { GroupStage } = this.state;
+    this.timerID = setInterval(
+      () => {
+        this.runNextGroupStageGame();
+      },
+      1500
+    );
+  }
+
+  runNextGroupStageGame() {
+    const { groupStage, nextGroupStageGame, groupMatchResult } = this.state;
+    const matchResult1 = groupStage.nextGame(nextGroupStageGame);
+    const matchResult2 = groupStage.nextGame(nextGroupStageGame + 1);
     this.setState({
-      standings: GroupStage.startGame(),
-      startKnockout: true
+      standings: groupStage.calcStandings(),
+      nextGroupStageGame: nextGroupStageGame + 2,
+      groupMatchResult: groupMatchResult.concat([matchResult1, matchResult2])
     });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { groupStage, nextGroupStageGame } = this.state;
+    if (nextGroupStageGame > prevState.nextGroupStageGame &&
+      nextGroupStageGame === groupStage.schedule.length) {
+      clearInterval(this.timerID);
+      this.setState({
+        startKnockout: true
+      });
+    }
   }
 
   handleStartKnockout = () => {
@@ -250,21 +336,49 @@ class TournamentMatch extends Component {
   }
 
   render() {
-    const { standings, GroupStage, startKnockout } = this.state;
+    const {
+      standings,
+      groupStage,
+      groupMatchResult,
+      startKnockout
+    } = this.state;
     const groupID = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
     return (
       <div className="tournament">
+        {
+          groupMatchResult.length > 0 &&
+          <div className="tournament__group-match">
+            <h2 className="tournament__group-match-title">Match Result</h2>
+            {
+              groupMatchResult.map((matchResult, index) => {
+                return (
+                  <div key={index} className="tournament__group-match-result">
+                    <p className="tournament__group-match-team">
+                      {matchResult.home}
+                    </p>
+                    <p className="tournament__group-match-score">
+                      {matchResult.homeScore} : {matchResult.awayScore} 
+                    </p>
+                    <p className="tournament__group-match-team">
+                      {matchResult.away}
+                    </p>
+                  </div>
+                );
+              })
+            }
+          </div>
+        }
         <div className="tournament__group">
           {
             groupID.map((group, index) => {
               return (
-                GroupStage &&
+                groupStage &&
                 <div key={index} className="tournament__group-standings">
                   <h2 className="tournament__group-name">Group {group}</h2>
                   {
                     this.createStandings(
                       standings[group],
-                      GroupStage.teams[group]
+                      groupStage.teams[group]
                     )
                   }
                 </div>
